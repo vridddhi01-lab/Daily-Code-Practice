0f546a7f911fd66112b3e76ed0a05ba23935b1d6
Vridhi <vridddhi.01@gmail.com>
2026-03-12 15:59:02 +0530

    changes done

--- Vridhi_sumofnnatural_q3.c
+++ Vridhi_sumofnnatural_q3.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 int main(){
     int n;
     int sum = 0;
-    scanf("%d", &n);
+    scanf("%d", &n);    
     if (n>=0){
         for(int i = 1; i <= n; i++ ){
             sum += i;
-        }}else{
+        }}
+        else{
             printf("negative number not allowed.");
         }
         printf("sum = %d",sum);
